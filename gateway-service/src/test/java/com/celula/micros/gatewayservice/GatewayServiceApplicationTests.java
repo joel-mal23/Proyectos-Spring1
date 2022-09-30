afbf8b9b151bfe29ebf6813925b4feffa355c377
joel-mal23 <joel.malvaez.23@gmail.com>
2022-09-30 15:06:45 -0500

    gateway-service

---
+++ gateway-service/src/test/java/com/celula/micros/gatewayservice/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.celula.micros.gatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
