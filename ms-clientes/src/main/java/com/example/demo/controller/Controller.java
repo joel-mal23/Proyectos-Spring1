01f1549e0eaa63900fb0f22187c3d32d3f873b17
joel-mal23 <joel.malvaez.23@gmail.com>
2022-10-04 09:54:42 -0500

    jj

--- ms-clientes/src/main/java/com/example/demo/controller/Controller.java
+++ ms-clientes/src/main/java/com/example/demo/controller/Controller.java
@@ -1,8 +1,5 @@
 package com.example.demo.controller;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,38 +17,5 @@ public class Controller {
 		
 		
 	}
-=======
-=======
->>>>>>> 21fd422ccbd9e8e5a3168268e5671fb1ca2ab668
-=======
->>>>>>> 21fd422ccbd9e8e5a3168268e5671fb1ca2ab668
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.http.HttpStatus;
-
-
-@RestController
-@RequestMapping("/ms-clientes")
-
-public class Controller {
-	
-	@GetMapping("/cli")
-	public ResponseEntity<?> responseEntity(){
-		
-	
-		return new ResponseEntity<>("VO Clientes", HttpStatus.OK);
-	}	
-	
-	
-
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> 21fd422ccbd9e8e5a3168268e5671fb1ca2ab668
-=======
->>>>>>> 21fd422ccbd9e8e5a3168268e5671fb1ca2ab668
-=======
->>>>>>> 21fd422ccbd9e8e5a3168268e5671fb1ca2ab668
 
 }
