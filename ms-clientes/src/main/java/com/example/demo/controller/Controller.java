8d5be3edc1b31776f8c728987927a6a6cd144a13
joel-mal23 <joel.malvaez.23@gmail.com>
2022-10-04 09:30:18 -0500

    commit

--- ms-clientes/src/main/java/com/example/demo/controller/Controller.java
+++ ms-clientes/src/main/java/com/example/demo/controller/Controller.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+<<<<<<< HEAD
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,5 +18,28 @@ public class Controller {
 		
 		
 	}
+=======
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.HttpStatus;
+
+
+@RestController
+@RequestMapping("/ms-clientes")
+
+public class Controller {
+	
+	@GetMapping("/cli")
+	public ResponseEntity<?> responseEntity(){
+		
+	
+		return new ResponseEntity<>("VO Clientes", HttpStatus.OK);
+	}	
+	
+	
+
+>>>>>>> 21fd422ccbd9e8e5a3168268e5671fb1ca2ab668
 
 }
