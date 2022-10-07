05a0b3e43a9bb668cf51943a6e7269377baa93cf
joel-mal23 <joel.malvaez.23@gmail.com>
2022-10-06 19:04:20 -0500

    connfiguracion ms-seguros-ultime

--- ms-seguros/src/main/java/com/example/demo/openfeingempleaos/EmpleadosOpenFeing.java
+++ ms-seguros/src/main/java/com/example/demo/openfeingempleaos/EmpleadosOpenFeing.java
@@ -3,7 +3,7 @@ package com.example.demo.openfeingempleaos;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name="ms-empleados",url = "http://localhost:8088/ms-empleados")
+@FeignClient(name="ms-empleados",url = "http://localhost:8486/ms-empleados")
 public interface EmpleadosOpenFeing {
 	
 	@GetMapping("/empl")
